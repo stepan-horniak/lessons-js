dbaa4c4d1c45d12e3e39bf281027f0ef34a8c8d9
stepan <stepanhornjak@gmail.com>
2025-01-09 15:11:16 +0200

    lesson-11

--- lesson-11/task/task-2/scripts/scrypt.js
+++ lesson-11/task/task-2/scripts/scrypt.js
@@ -1,51 +1,104 @@
 "usestrict"
 const profits = [
-  [1200, 1500, 1100, 1400, 2000, 1700, 1600],
-  [900, 800, 1000, 950, 1200, 1150, 1100],
-  [2200, 2100, 2300, 2400, 2500, 2600, 2700],
+  [200, 1500, 1100, 1400, 50, 1700, 1600],
+  [900, 800, 1000, 950, 1200, 50, 1100],
+  [20, 2100, 300, 400, 500, 600, 700],
   [500, 700, 800, 750, 600, 650, 700],
-  [1300, 1400, 1350, 1450, 1500, 1550, 1600],
-  [1700, 1800, 1750, 1650, 1600, 1550, 1500],
-  [800, 850, 900, 950, 1000, 1050, 1100],
-  [1900, 2000, 1950, 1850, 1800, 1750, 1700],
+  [100, 400, 150, 1450, 50, 50, 1600],
+  [1700, 1800, 100, 1650, 600, 50, 1500],
+  [800, 850, 900, 50, 100, 1050, 100],
+  [100, 200, 190, 50, 1800, 750, 1700],
 ]
 
 //==========1=============
-let totalProfitPerWeek = (arr) => {
-  let profit = arr.forEach((el) => el.reduce((prevVal, el) => prevVal + el))
-  return profit
-}
+
+const totalProfitPerWeek = profits.map((el) =>
+  el.reduce((prevValue, el) => el + prevValue)
+)
 
 //==========2=============
-let totalProfitByDay = (arr) => {
-  let profit = profits.map((store) =>
-    store.reduce((sum, profit) => sum + profit, 0)
-  )
 
-  return profit
+function totalProfitByDay(arr) {
+  let profitsPerDay = new Array(7).fill(0)
+  for (let row = 0; row < arr.length; row++) {
+    for (let col = 0; col < arr[row].length; col++) {
+      profitsPerDay[col] += arr[row][col]
+    }
+  }
+  return profitsPerDay
 }
-console.log(totalProfitByDay(profits))
-// console.log();
 
-//=========3=============
-//==========4=============
+//=========3==4=========
+
+function totalProfitWorkingAndWeekendDay(arr, dayNumStart, dayNumEnd) {
+  let totalSum = 0
+  for (let row = 0; row < arr.length; row++) {
+    for (let col = dayNumStart; col <= dayNumEnd; col++) {
+      totalSum += arr[row][col]
+    }
+  }
+  return totalSum
+}
+
 //==========5=============
+
+function maximumProfitForWednesday(arr) {
+  return Math.max(...arr.map((row) => row[2]))
+}
+
 //==========6=============
+
+function formListGreaterThan200(arr) {
+  return arr.flat().filter((el) => el > 200)
+}
+
 //==========7=============
+
+function sortWeekByGrowth(arr) {
+  let sortArr = arr.map((el) => [...el].sort((a, b) => a - b))
+  return sortArr
+}
+
+//================================
+function showTaksSort(resultSort) {
+  let result = ``
+  for (const element of resultSort) {
+    result += `<div>[${element}]</div>`
+  }
+  return result
+}
 //==========8=============
+
+function sortMaxElementInRow(arr) {
+  return [...arr].sort((a, b) => Math.max(...a) - Math.max(...b))
+}
+
 //==========9=============
 
-document.querySelector(".answer").innerHTML = `
-<ol>
-<li>${totalProfitPerWeek(profits)}</li>
-</ol>`
-{
-  /* <li>${}</li>
-<li>${}</li>
-<li>${}</li>
-<li>${}</li>
-<li>${}</li>
-<li>${}</li>
-<li>${}</li>
-<li>${}</li> */
+function sortSumMaxElementInRow(arr) {
+  let sortArr = [...arr].sort(
+    (a, b) =>
+      a.reduce((prevValue, el) => el + prevValue) -
+      b.reduce((prevValue, el) => el + prevValue)
+  )
+
+  return sortArr
 }
+
+//=============================
+document.querySelector(".answer").innerHTML = `
+<div>початковий масив чосел : ${showTaksSort(profits)}</div>
+<ol>
+
+<li>${totalProfitPerWeek}</li>
+<li>${totalProfitByDay(profits)}</li>
+<li>${totalProfitWorkingAndWeekendDay(profits, 0, 4)}</li>
+<li>${totalProfitWorkingAndWeekendDay(profits, 5, 6)}</li>
+<li>${maximumProfitForWednesday(profits)}</li>
+<li>[${formListGreaterThan200(profits)}]</li>
+<li>${showTaksSort(sortWeekByGrowth(profits))}</li>
+ <li>${showTaksSort(sortMaxElementInRow(profits))}</li>  
+ <li>${showTaksSort(sortSumMaxElementInRow(profits))}</li>  
+
+
+</ol>`
--- lesson-11/task/task-3/scripts/scrypt.js
+++ lesson-11/task/task-3/scripts/scrypt.js
@@ -1,1 +1,99 @@
 "usestrict"
+if (confirm("почнемо гру")) {
+  let number = 5
+
+  function randomNumber(min = 0, max = number) {
+    return min + Math.floor(Math.random() * (max - min + 1))
+  }
+
+  function generationField() {
+    let field = []
+    for (let row = 0; row <= number; row++) {
+      let arr = []
+      for (let col = 0; col <= number; col++) {
+        arr.push(0)
+      }
+      field.push(arr)
+    }
+    return field
+  }
+
+  function generationRandomShipInField(field) {
+    let ship = 5
+
+    while (ship) {
+      let randPositioRow = randomNumber()
+      let randPositioColumn = randomNumber()
+      if (field[randPositioRow][randPositioColumn] === 1) {
+        continue
+      } else {
+        field[randPositioRow][randPositioColumn] = 1
+        ship--
+      }
+    }
+    return field
+  }
+
+  let field = generationRandomShipInField(generationField())
+
+  function showArray(arr) {
+    let result = ``
+    for (const element of arr) {
+      result += `[${element}]`
+    }
+
+    return result
+  }
+  //=======================================
+  let countShip = 5,
+    countShot = 25
+
+  while (countShip) {
+    let userNumRow =
+      parseInt(
+        prompt(
+          `${showArray(field)}введіть номер рядка від 1 до ${
+            number + 1
+          } залишилось пострілів : ${countShot}`
+        )
+      ) - 1
+    let userNumColumn =
+      parseInt(
+        prompt(
+          `введіть номер колонки від від 1 до ${
+            number + 1
+          } залишилось пострілів : ${countShot}`
+        )
+      ) - 1
+    console.log(userNumColumn)
+
+    if (
+      userNumRow < 0 ||
+      userNumRow > number ||
+      userNumColumn < 0 ||
+      userNumColumn > number ||
+      userNumRow === null ||
+      userNumColumn === null ||
+      isNaN(userNumRow) ||
+      isNaN(userNumColumn)
+    ) {
+      alert("введені некоректрі дані")
+      continue
+    }
+
+    if (countShot === 0) {
+      alert("У вас закінчилися постріли. Наступного разу пощастить!")
+      break
+    }
+    if (field[userNumRow][userNumColumn] === 1) {
+      alert("попав")
+      field[userNumRow][userNumColumn] = 0
+      countShip--
+    } else {
+      if (!confirm("не попав, продовджимо")) break
+    }
+    countShot--
+  }
+  if (countShip === 0) alert("вітаємо")
+  else alert("наступного разу пощастить")
+}
