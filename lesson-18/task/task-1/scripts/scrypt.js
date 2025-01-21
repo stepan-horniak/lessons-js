06700b9ccd8f3d1256a3db2da7be427486913101
stepan <stepanhornjak@gmail.com>
2025-01-21 19:03:07 +0200

    lesson-18

---
+++ lesson-18/task/task-1/scripts/scrypt.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const firstNumber = parseFloat(document.getElementById("first-number").value)
+const secondNumber = parseFloat(document.getElementById("second-number").value)
+
+function add() {
+  const firstNumber = parseFloat(document.getElementById("first-number").value)
+  const secondNumber = parseFloat(
+    document.getElementById("second-number").value
+  )
+}
+
+window.onload = function () {
+  //   document.getElementById("add").onclick =
+}
