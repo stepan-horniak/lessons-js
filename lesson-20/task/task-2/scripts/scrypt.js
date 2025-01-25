4b77ea4389ace3420b73129801d27e95302a0cfc
stepan <stepanhornjak@gmail.com>
2025-01-25 15:34:54 +0200

    lesson-20

--- lesson-20/task/task-2/scripts/scrypt.js
+++ lesson-20/task/task-2/scripts/scrypt.js
@@ -1,60 +1,48 @@
 window.addEventListener("load", () => {
   class Star {
-    constructor(container) {
-      this.container = container
-      this.createStar()
+    constructor(numberStar) {
+      this.numberStar = numberStar
+    }
+
+    randomNumber(max = 100, min = 0) {
+      return min + Math.floor(Math.random() * max)
     }
 
     createStar() {
       const divStar = document.createElement("div")
-      divStar.style.cssText = `
-        width: 30px;
-        height: 30px;
-        background-color: gold;
-        clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 
-                           79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
-        position: absolute;
-        top: ${this.randomPosition()}%;
-        left: ${this.randomPosition()}%;
-        transition: top 0.5s, left 0.5s, width 0.5s, height 0.5s, opacity 0.5s;
-        opacity: 1;
-      `
-      this.container.append(divStar)
+      const randomSize = this.randomNumber()
+      divStar.style.backgroundColor = "gold"
+      divStar.style.clipPath =
+        "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%,79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)"
+      divStar.style.position = "absolute"
+      divStar.style.width = `${randomSize}px`
+      divStar.style.height = `${randomSize}px`
+      divStar.style.top = `${this.randomNumber()}%`
+      divStar.style.left = `${this.randomNumber()}%`
+      document.querySelector(".container").append(divStar)
       this.starEl = divStar
-      this.animateStar()
     }
-
-    randomPosition(max = 100, min = 0) {
-      return min + Math.floor(Math.random() * max)
-    }
-
-    animateStar() {
-      let size = 30
-      this.starEl.style.opacity = "0"
-
-      setTimeout(() => {
-        size = 30
-        this.starEl.style.top = `${this.randomPosition()}%`
-        this.starEl.style.left = `${this.randomPosition()}%`
-        this.starEl.style.width = `${size}px`
-        this.starEl.style.height = `${size}px`
-
-        this.starEl.style.opacity = "1"
-      }, this.randomPosition(1000, 10))
-    }
+    // animateStar() {
+    //   this.createStar()
+    //   let size = 0
+    //   setInterval(() => {
+    //     size++
+    //     this.starEl.style.width = `${size}px`
+    //     this.starEl.style.height = `${size}px`
+    //     if (size === 50) {
+    //       this.starEl.style.opacity = "0"
+    //       this.starEl.style.top = `${this.randomNumber()}%`
+    //       this.starEl.style.left = `${this.randomNumber()}%`
+    //     }
+    //     if (size === 53) {
+    //       this.starEl.style.opacity = "1"
+    //       size = 0
+    //     }
+    //   }, 100)
+    // }
   }
-
-  const container = document.createElement("div")
-  container.style.cssText = `
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #282c34;
-    z-index: -1;
-  `
-  document.body.append(container)
-
-  new Star(container)
+  for (let i = 0; i < 10; i++) {
+    const star = new Star()
+    star.createStar()
+  }
 })
